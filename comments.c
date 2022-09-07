480c80006f5a5ca3718ae7e553031154a3e40678
Michael Onyeweke <michaelonyeweke@yahoo.com>
2022-09-07 17:52:07 +0100

    amended comments code

--- comments.c
+++ comments.c
@@ -1,31 +1,20 @@
 #include "comments.h"
-#include "io.h"
-#include <stdlib.h>
+
 
 /**
  * ex_comment - removes comment from input string
  * @line: input string
- *
- * Return: strings from the input without
- *         the comment and characters after it
  */
 char *ex_comment(char *line)
 {
 	int i;
 
-	while (line[i] != '\0')
+	for (i = 0; line[i] != '0'; i++)
 	{
-		for (i = 0; line[i]; i++)
+		if (line[i] == '#')
 		{
-			if (line[i] == '#')
-			{
-				if (line[i + 1])
-				{
-					line[i + 1] = '\0';
-					continue;
-				}
-				return (NULL);
-			}
+			line[i] = '\0';
+			break;
 		}
 	}
 	return (line);
